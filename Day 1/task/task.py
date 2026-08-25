b5d7f985fc0d59537e9de86cda3008da93108c98
koo min ru <koominru@hotmail.com>
2026-08-26 01:11:02 +0800

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,9 @@
+print("Welcome to Band Name Generator.")
+
+country = (
+    input("What's the name of the country you grew up in?\n"))
+
+pet = (
+    input("What's your pet's name?\n"))
+
+print("Your band name could be " + country + " " + pet)
