492d8c8539ebdea0af9d1ae829ed71092ace9f09
ru691 <koominru@hotmail.com>
2026-08-30 20:12:48 +0800

    functions

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,5 @@
+def my_function(): #def = define/create a function, whatever comes after that is the function im gonna create
+    print("Hello World")
+    print("Hello Again")
+    print("I like typing")
+my_function() 
