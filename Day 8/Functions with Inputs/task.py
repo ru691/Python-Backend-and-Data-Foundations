27afcdcc6ad454e3d4189aa6a42e4c036efd89d7
ru691 <koominru@hotmail.com>
2026-09-06 23:41:38 +0800

    Caesar Cipher using def function

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,11 @@
+def greet():
+    print("hello")
+    print("How are you?")
+    print("Goodbye")
+
+greet()
+
+def greet_with_name(name): #name = parameter
+    print(f"Hello {name}")
+    print(f"How are you {name}?")
+greet_with_name("kei") #kei = argument. parameter = argument, name = kei
---
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -0,0 +1,14 @@
+# Functions with input
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
+
+# FUNCTIONS WITH MORE THAN 1 INPUT
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+greet_with(location = "Malaysia", name = "Jack")
